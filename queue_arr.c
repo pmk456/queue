6f177e5094778bfc3e2aa09a7ac345d720ba7b9e
Pmk456 <45127373+pmk456@users.noreply.github.com>
2022-04-15 17:06:16 +0530

    QUEUE DATA STRUCTURE IMPLEMENTED USING ARRAY

---
+++ queue_arr.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 3
+int arr[MAX];
+int rear = -1;
+int front = -1;
+
+void insert(int data);
+int del();
+int peek();
+int isEmpty();
+int isFull();
+void display();
+int main(){
+    int op, data;
+    printf("1.Insert\n2.Delete\n3.Display Top Element");
+    printf("\n4.Display All Queue Elements\n5.Exit\n");
+    do{
+        printf("Enter option: ");
+        scanf("%d", &op);
+        switch(op){
+            case 1:
+                printf("Enter Data: ");
+                scanf("%d", &data);
+                insert(data);
+                break;
+            case 2:
+                data = del();
+                if (data != -1) printf("Popped item is %d\n", data);
+                break;
+            case 3:
+                data = peek();
+                if(data != -1) printf("Top Element is %d\n", data);
+                break;
+            case 4:
+                display();
+                break;
+            case 5:
+                exit(0);
+            default:
+                printf("Enter Correct Option!\n");
+        }
+    }while(op != 5);
+}
+int isFull(){
+    if(rear == MAX - 1) return 1;
+    return 0;
+}
+int isEmpty(){
+    if(front == -1 || front == rear + 1) return 1;
+    return 0;
+}
+void insert(int data){  
+    if(isFull()){
+        printf("Queue Overflow!\n");
+        return;
+    }
+    if(front == -1) front = 0;
+    arr[++rear] = data;
+}
+int del(){
+    if(isEmpty()){
+        printf("Queue Underflow!\n");
+        return -1;
+    }
+    return arr[front++];
+}
+int peek(){
+   if(isEmpty()){
+       printf("Queue Underflow\n");
+       return -1;
+   }
+   return arr[front];
+}
+void display(){
+    int i;
+    for(i=front;i<=rear;i++) printf("%d\t", arr[i]);
+    printf("\n"); 
+}
+
